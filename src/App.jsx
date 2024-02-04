716c85edd1c1024364e89e86da51dd8d6405e45f
Sayyucha <119330669+Sayyucha@users.noreply.github.com>
2024-02-04 23:04:08 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./Pages/Home"
+import Signin from "./Pages/Signin"
+import SignUp from "./Pages/SignUp"
+import About from "./Pages/About"
+import Profile from "./Pages/Profile"
+import Header from "./components/Header"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/sign-in" element={<Signin />}/>
+        <Route path="/sign-up" element={<SignUp />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
